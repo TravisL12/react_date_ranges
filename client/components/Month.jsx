2ed0eae6d137a71a3cb69dfd884b53d624c19d43
Travis Lawrence <tlawrence@tumblr.com>
2017-03-07 14:53:44 -0800

    Move weeks loop to Month

--- client/components/Month.jsx
+++ client/components/Month.jsx
@@ -9,6 +9,19 @@ const monthNames = [
 
 const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+function chunkWeeks (dates) {
+    let weeks = [],
+        daysInWeek = 7,
+        weekCount = Math.ceil(dates.length / daysInWeek);
+
+    for (var i = 0; i < weekCount; i++) {
+        let weekIdx = i * daysInWeek;
+        let days = dates.slice(weekIdx, weekIdx + daysInWeek);
+        weeks.push(days);
+    }
+    return weeks;
+}
+
 export default function Month (props) {
     let monthName = monthNames[props.month];
 
@@ -16,7 +29,11 @@ export default function Month (props) {
         <div key={monthName + props.year} className={'month ' + monthName.toLowerCase()}>
             <div className='month--name'>{monthName + ' ' + props.year}</div>
             <div className='month--header'>{dayNames.map((day) => { return <WeekHeader key={day} day={day}/> })}</div>
-            <Week dates={props.dates}/>
+            <div className='month--weeks'>{
+                chunkWeeks(props.dates).map((dates, i) => {
+                    return <Week key={i} dates={dates} idx={i}/>
+                })
+            }</div>
         </div>
     )
 }
--- client/components/Week.jsx
+++ client/components/Week.jsx
@@ -1,32 +1,11 @@
 import React from 'react';
 
-function chunkWeeks (dates) {
-    let weeks = [],
-        daysInWeek = 7,
-        weekCount = Math.ceil(dates.length / daysInWeek);
-
-    for (var i = 0; i < weekCount; i++) {
-        let weekIdx = i * daysInWeek;
-        let days = dates.slice(weekIdx, weekIdx + daysInWeek);
-        weeks.push(days);
-    }
-    return weeks;
-}
-
-function buildDays (month) {
-    return month.map((date, j) => {
+function buildDays (dates) {
+    return dates.map((date, j) => {
         return date === null ? <li key={j} className='week--tile none'></li> : date;
     });
 }
 
-function buildWeeks (dates) {
-    return chunkWeeks(dates).map((month, i) => {
-        return <ul key={i} className={'week week-' + (i + 1)}>{buildDays(month)}</ul>;
-    });
-}
-
 export default function Week (props) {
-    return (
-        <div className='month--weeks'>{buildWeeks(props.dates)}</div>
-    )
+    return <ul key={props.idx} className={'week week-' + (props.idx + 1)}>{buildDays(props.dates)}</ul>
 }
