bde5c50b454e0bd2ceea0f047c83250816294cc0
Travis Lawrence <tlawrence@tumblr.com>
2018-10-04 16:03:22 -0700

    Blah

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import Year from "./Year";
 
 function LandingPage(props) {
-  const years = Object.keys(props.spending).map((year, idx) => {
-    return (
-      <div key={idx}>
-        <h2>{year}</h2>
-        <Year yearSpending={props.spending[year]} year={year} />
-      </div>
-    );
-  });
-
-  return <div className="year-tiles">{years}</div>;
+  return (
+    <div className="year-tiles">
+      {Object.keys(props.spending).map((year, idx) => {
+        return (
+          <div key={idx}>
+            <h2>{year}</h2>
+            <Year yearSpending={props.spending[year]} year={year} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default LandingPage;
